1c246e32e35bc343ee7afa8b7ec701994f09a087
thamannaa <77442887+thamannaa@users.noreply.github.com>
2021-05-22 17:01:16 +0530

    Add files via upload

---
+++ quiz_page.js
@@ -0,0 +1,9 @@
+player1_name=localStorage.getItem("player1_name");
+player2_name=localStorage.getItem("player2_name");
+player1_score=0;
+player2_score=0;
+document.getElementById("player1_name").ineerHTML=player1_name+":";
+document.getElementById("player2_name").ineerHTML=player2_name+":";
+document.getElementById("player1_score").ineerHTML=player1_score;
+document.getElementById("player2_score").ineerHTML=player2_score;
+
